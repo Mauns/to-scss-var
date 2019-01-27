f6f1ef845b20b3ca655b54ffbc9206d1c10ce677
Mauns <maunsbruns@gmx.de>
2019-01-28 00:11:03 +0100

    init

---
+++ src/to-scss-var.js
@@ -0,0 +1,87 @@
+import sketch from 'sketch'
+
+const fs = require('@skpm/fs')
+
+// documentation: https://developer.sketchapp.com/reference/api/
+
+export default function() {  
+
+  const Document = require('sketch/dom').Document
+  let   document = Document.getSelectedDocument()
+  let   page = document.selectedPage
+  let   selection = document.selectedLayers
+  let   layerStyles = document.getSharedLayerStyles()
+
+  let   colors = []
+  let   ColorArray = []
+  let   NameList = []
+
+  // Iterate over Layerstyles
+  layerStyles.forEach((style, i) => {
+
+    let styleName = style.name
+    let splittedName
+    let fills = style.style.fills
+
+    // Iterate over fills of styles and find the enabled ones
+    fills.forEach((fill, i) => {
+      if (fill.enabled === true) {
+        if (fill.type == 'Fill') {
+
+        let color = fill.color
+
+        // parse name 
+        styleName = styleName.toLowerCase()
+        styleName = styleName.split("/")
+        splittedName = styleName
+        styleName = styleName.length > 1 ? styleName[styleName.length - 1] : styleName
+
+        splittedName.pop()
+        splittedName = splittedName.join("/")
+
+        let obj = {}
+            obj = {
+              path: splittedName,
+              name: styleName,
+              color
+            }
+
+        return colors.push(obj), styleName, splittedName
+        }
+      }
+    })
+  })
+
+  colors.forEach((d, i) => {
+    
+    let string = `// ${d.path}\n$${d.name}: ${d.color};\n`
+    
+    return ColorArray.push(string)
+  })
+
+  
+  ColorArray.sort()
+  ColorArray = ColorArray.toString()
+  ColorArray = ColorArray.split(",")
+  
+  colors.forEach((d, i) => {
+    if (ColorArray[i].includes(`// ${d.path}`)) {
+      return ColorArray[i].replace(`// ${d.path}`, 'lol')
+    }
+  })
+
+  ColorArray = ColorArray.join("\n")
+  
+  let fileContent = ColorArray
+
+  fs.writeFileSync("/Users/marvinbruns/Desktop/fills.scss", fileContent)
+
+  sketch.UI.message("generated scss file. Take a look at your desktop.")
+}
+
+function uniq(a) {
+  let seen = {}
+  return a.filter(function (item) {
+    seen.hasOwnProperty(item) ? false : (seen[item] = true)
+  })
+}
---
+++ to-scss-var.sketchplugin/Contents/Sketch/to-scss-var.js
@@ -0,0 +1,587 @@
+var that = this;
+function __skpm_run (key, context) {
+  that.context = context;
+
+var exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/to-scss-var.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./node_modules/@skpm/fs/index.js":
+/*!****************************************!*\
+  !*** ./node_modules/@skpm/fs/index.js ***!
+  \****************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// TODO: async. Should probably be done with NSFileHandle and some notifications
+// TODO: file descriptor. Needs to be done with NSFileHandle
+var Buffer = __webpack_require__(/*! buffer */ "buffer").Buffer
+
+var ERROR_MESSAGES = {
+  '-2': 'no such file or directory',
+  '-13': 'permission denied',
+  '-21': 'illegal operation on a directory'
+}
+
+function fsError(options) {
+  var error = new Error(
+    options.code + ': '
+    + ERROR_MESSAGES[options.errno] + ', '
+    + options.syscall
+    + (options.path ? ' \'' + options.path + '\'' : '')
+  )
+
+  Object.keys(options).forEach(function (k) {
+    error[k] = options[k]
+  })
+
+  return error
+}
+
+function encodingFromOptions(options, defaultValue) {
+  return options && options.encoding
+    ? String(options.encoding)
+    : (
+      options
+        ? String(options)
+        : defaultValue
+    )
+}
+
+module.exports.constants = {
+  F_OK: 0,
+  R_OK: 4,
+  W_OK: 2,
+  X_OK: 1
+}
+
+module.exports.accessSync = function(path, mode) {
+  mode = mode | 0
+  var fileManager = NSFileManager.defaultManager()
+
+  switch (mode) {
+    case 0:
+      canAccess = module.exports.existsSync(path)
+      break
+    case 1:
+      canAccess = Boolean(Number(fileManager.isExecutableFileAtPath(path)))
+      break
+    case 2:
+      canAccess = Boolean(Number(fileManager.isWritableFileAtPath(path)))
+      break
+    case 3:
+      canAccess = Boolean(Number(fileManager.isExecutableFileAtPath(path))) && Boolean(Number(fileManager.isWritableFileAtPath(path)))
+      break
+    case 4:
+      canAccess = Boolean(Number(fileManager.isReadableFileAtPath(path)))
+      break
+    case 5:
+      canAccess = Boolean(Number(fileManager.isReadableFileAtPath(path))) && Boolean(Number(fileManager.isExecutableFileAtPath(path)))
+      break
+    case 6:
+      canAccess = Boolean(Number(fileManager.isReadableFileAtPath(path))) && Boolean(Number(fileManager.isWritableFileAtPath(path)))
+      break
+    case 7:
+      canAccess = Boolean(Number(fileManager.isReadableFileAtPath(path))) && Boolean(Number(fileManager.isWritableFileAtPath(path))) && Boolean(Number(fileManager.isExecutableFileAtPath(path)))
+      break
+  }
+
+  if (!canAccess) {
+    throw new Error('Can\'t access ' + String(path))
+  }
+}
+
+module.exports.appendFileSync = function(file, data, options) {
+  if (!module.exports.existsSync(file)) {
+    return module.exports.writeFileSync(file, data, options)
+  }
+
+  var handle = NSFileHandle.fileHandleForWritingAtPath(file)
+  handle.seekToEndOfFile()
+
+  var encoding = encodingFromOptions(options, 'utf8')
+
+  var nsdata = Buffer.from(data, encoding === 'NSData' || encoding === 'buffer' ? undefined : encoding).toNSData()
+
+  handle.writeData(nsdata)
+}
+
+module.exports.chmodSync = function(path, mode) {
+  var err = MOPointer.alloc().init()
+  var fileManager = NSFileManager.defaultManager()
+  fileManager.setAttributes_ofItemAtPath_error({
+    NSFilePosixPermissions: mode
+  }, path, err)
+
+  if (err.value() !== null) {
+    throw new Error(err.value())
+  }
+}
+
+module.exports.copyFileSync = function(path, dest, flags) {
+  var err = MOPointer.alloc().init()
+  var fileManager = NSFileManager.defaultManager()
+  fileManager.copyItemAtPath_toPath_error(path, dest, err)
+
+  if (err.value() !== null) {
+    throw new Error(err.value())
+  }
+}
+
+module.exports.existsSync = function(path) {
+  var fileManager = NSFileManager.defaultManager()
+  return Boolean(Number(fileManager.fileExistsAtPath(path)))
+}
+
+module.exports.linkSync = function(existingPath, newPath) {
+  var err = MOPointer.alloc().init()
+  var fileManager = NSFileManager.defaultManager()
+  fileManager.linkItemAtPath_toPath_error(existingPath, newPath, err)
+
+  if (err.value() !== null) {
+    throw new Error(err.value())
+  }
+}
+
+module.exports.mkdirSync = function(path, mode) {
+  mode = mode || 0o777
+  var err = MOPointer.alloc().init()
+  var fileManager = NSFileManager.defaultManager()
+  fileManager.createDirectoryAtPath_withIntermediateDirectories_attributes_error(path, false, {
+    NSFilePosixPermissions: mode
+  }, err)
+
+  if (err.value() !== null) {
+    throw new Error(err.value())
+  }
+}
+
+module.exports.mkdtempSync = function(path) {
+  function makeid() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 6; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+  }
+  var tempPath = path + makeid()
+  module.exports.mkdirSync(tempPath)
+  return tempPath
+}
+
+module.exports.readdirSync = function(path) {
+  var fileManager = NSFileManager.defaultManager()
+  var paths = fileManager.subpathsAtPath(path)
+  var arr = []
+  for (var i = 0; i < paths.length; i++) {
+    arr.push(String(paths[i]))
+  }
+  return arr
+}
+
+module.exports.readFileSync = function(path, options) {
+  var encoding = encodingFromOptions(options, 'buffer')
+  var fileManager = NSFileManager.defaultManager()
+  var data = fileManager.contentsAtPath(path)
+  if (!data) {
+    var doesExist = fileManager.fileExistsAtPath(path)
+    if (!doesExist) {
+      throw fsError({
+        errno: -2,
+        code: 'ENOENT',
+        syscall: 'open',
+        path: path
+      })
+    }
+    var isReadable = fileManager.isReadableFileAtPath(path)
+    if (!isReadable) {
+      throw fsError({
+        errno: -13,
+        code: 'EACCES',
+        syscall: 'open',
+        path: path
+      })
+    }
+    var isDirectory = fileManager.fileExistsAtPath_isDirectory(path, true)
+    if (isDirectory) {
+      throw fsError({
+        errno: -21,
+        code: 'EISDIR',
+        syscall: 'read'
+      })
+    }
+    throw new Error('Unknown error while reading file ' + path)
+  }
+
+  var buffer = Buffer.from(data)
+
+  if (encoding === 'buffer') {
+    return buffer
+  } else if (encoding === 'NSData') {
+    return buffer.toNSData()
+  } else {
+    return buffer.toString(encoding)
+  }
+}
+
+module.exports.readlinkSync = function(path) {
+  var err = MOPointer.alloc().init()
+  var fileManager = NSFileManager.defaultManager()
+  var result = fileManager.destinationOfSymbolicLinkAtPath_error(path, err)
+
+  if (err.value() !== null) {
+    throw new Error(err.value())
+  }
+
+  return String(result)
+}
+
+module.exports.realpathSync = function(path) {
+  return String(NSString.stringByResolvingSymlinksInPath(path))
+}
+
+module.exports.renameSync = function(oldPath, newPath) {
+  var err = MOPointer.alloc().init()
+  var fileManager = NSFileManager.defaultManager()
+  fileManager.moveItemAtPath_toPath_error(oldPath, newPath, err)
+
+  var error = err.value()
+
+  if (error !== null) {
+    // if there is already a file, we need to overwrite it
+    if (String(error.domain()) === 'NSCocoaErrorDomain' && Number(error.code()) === 516) {
+      var err2 = MOPointer.alloc().init()
+      fileManager.replaceItemAtURL_withItemAtURL_backupItemName_options_resultingItemURL_error(NSURL.fileURLWithPath(newPath), NSURL.fileURLWithPath(oldPath), null, NSFileManagerItemReplacementUsingNewMetadataOnly, null, err2)
+      if (err2.value() !== null) {
+        throw new Error(err2.value())
+      }
+    } else {
+      throw new Error(error)
+    }
+  }
+}
+
+module.exports.rmdirSync = function(path) {
+  var err = MOPointer.alloc().init()
+  var fileManager = NSFileManager.defaultManager()
+  fileManager.removeItemAtPath_error(path, err)
+
+  if (err.value() !== null) {
+    throw new Error(err.value())
+  }
+}
+
+function parseStat(result) {
+  return {
+    dev: String(result.NSFileDeviceIdentifier),
+    // ino: 48064969, The file system specific "Inode" number for the file.
+    mode: result.NSFileType | result.NSFilePosixPermissions,
+    nlink: Number(result.NSFileReferenceCount),
+    uid: String(result.NSFileOwnerAccountID),
+    gid: String(result.NSFileGroupOwnerAccountID),
+    // rdev: 0, A numeric device identifier if the file is considered "special".
+    size: Number(result.NSFileSize),
+    // blksize: 4096, The file system block size for i/o operations.
+    // blocks: 8, The number of blocks allocated for this file.
+    atimeMs: Number(result.NSFileModificationDate.timeIntervalSince1970()) * 1000,
+    mtimeMs: Number(result.NSFileModificationDate.timeIntervalSince1970()) * 1000,
+    ctimeMs: Number(result.NSFileModificationDate.timeIntervalSince1970()) * 1000,
+    birthtimeMs: Number(result.NSFileCreationDate.timeIntervalSince1970()) * 1000,
+    atime: new Date(Number(result.NSFileModificationDate.timeIntervalSince1970()) * 1000 + 0.5), // the 0.5 comes from the node source. Not sure why it's added but in doubt...
+    mtime: new Date(Number(result.NSFileModificationDate.timeIntervalSince1970()) * 1000 + 0.5),
+    ctime: new Date(Number(result.NSFileModificationDate.timeIntervalSince1970()) * 1000 + 0.5),
+    birthtime: new Date(Number(result.NSFileCreationDate.timeIntervalSince1970()) * 1000 + 0.5),
+    isBlockDevice: function() { return result.NSFileType === NSFileTypeBlockSpecial },
+    isCharacterDevice: function() { return result.NSFileType === NSFileTypeCharacterSpecial },
+    isDirectory: function() { return result.NSFileType === NSFileTypeDirectory },
+    isFIFO: function() { return false },
+    isFile: function() { return result.NSFileType === NSFileTypeRegular },
+    isSocket: function() { return result.NSFileType === NSFileTypeSocket },
+    isSymbolicLink: function() { return result.NSFileType === NSFileTypeSymbolicLink },
+  }
+}
+
+module.exports.lstatSync = function(path) {
+  var err = MOPointer.alloc().init()
+  var fileManager = NSFileManager.defaultManager()
+  var result = fileManager.attributesOfItemAtPath_error(path, err)
+
+  if (err.value() !== null) {
+    throw new Error(err.value())
+  }
+
+  return parseStat(result)
+}
+
+// the only difference with lstat is that we resolve symlinks
+//
+// > lstat() is identical to stat(), except that if pathname is a symbolic
+// > link, then it returns information about the link itself, not the file
+// > that it refers to.
+// http://man7.org/linux/man-pages/man2/lstat.2.html
+module.exports.statSync = function(path) {
+  return module.exports.lstatSync(module.exports.realpathSync(path))
+}
+
+module.exports.symlinkSync = function(target, path) {
+  var err = MOPointer.alloc().init()
+  var fileManager = NSFileManager.defaultManager()
+  var result = fileManager.createSymbolicLinkAtPath_withDestinationPath_error(path, target, err)
+
+  if (err.value() !== null) {
+    throw new Error(err.value())
+  }
+}
+
+module.exports.truncateSync = function(path, len) {
+  var hFile = NSFileHandle.fileHandleForUpdatingAtPath(sFilePath)
+  hFile.truncateFileAtOffset(len || 0)
+  hFile.closeFile()
+}
+
+module.exports.unlinkSync = function(path) {
+  var err = MOPointer.alloc().init()
+  var fileManager = NSFileManager.defaultManager()
+  var result = fileManager.removeItemAtPath_error(path, err)
+
+  if (err.value() !== null) {
+    throw new Error(err.value())
+  }
+}
+
+module.exports.utimesSync = function(path, aTime, mTime) {
+  var err = MOPointer.alloc().init()
+  var fileManager = NSFileManager.defaultManager()
+  var result = fileManager.setAttributes_ofItemAtPath_error({
+    NSFileModificationDate: aTime
+  }, path, err)
+
+  if (err.value() !== null) {
+    throw new Error(err.value())
+  }
+}
+
+module.exports.writeFileSync = function(path, data, options) {
+  var encoding = encodingFromOptions(options, 'utf8')
+
+  var nsdata = Buffer.from(
+    data, encoding === 'NSData' || encoding === 'buffer' ? undefined : encoding
+  ).toNSData()
+
+  nsdata.writeToFile_atomically(path, true)
+}
+
+
+/***/ }),
+
+/***/ "./src/to-scss-var.js":
+/*!****************************!*\
+  !*** ./src/to-scss-var.js ***!
+  \****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var sketch__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! sketch */ "sketch");
+/* harmony import */ var sketch__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(sketch__WEBPACK_IMPORTED_MODULE_0__);
+
+
+var fs = __webpack_require__(/*! @skpm/fs */ "./node_modules/@skpm/fs/index.js"); // documentation: https://developer.sketchapp.com/reference/api/
+
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  var Document = __webpack_require__(/*! sketch/dom */ "sketch/dom").Document;
+
+  var document = Document.getSelectedDocument();
+  var page = document.selectedPage;
+  var selection = document.selectedLayers;
+  var layerStyles = document.getSharedLayerStyles();
+  var colors = [];
+  var ColorArray = [];
+  var NameList = []; // Iterate over Layerstyles
+
+  layerStyles.forEach(function (style, i) {
+    var styleName = style.name;
+    var splittedName;
+    var fills = style.style.fills; // Iterate over fills of styles and find the enabled ones
+
+    fills.forEach(function (fill, i) {
+      if (fill.enabled === true) {
+        if (fill.type == 'Fill') {
+          var color = fill.color; // parse name 
+
+          styleName = styleName.toLowerCase();
+          styleName = styleName.split("/");
+          splittedName = styleName;
+          styleName = styleName.length > 1 ? styleName[styleName.length - 1] : styleName;
+          splittedName.pop();
+          splittedName = splittedName.join("/");
+          var obj = {};
+          obj = {
+            path: splittedName,
+            name: styleName,
+            color: color
+          };
+          return colors.push(obj), styleName, splittedName;
+        }
+      }
+    });
+  });
+  colors.forEach(function (d, i) {
+    var string = "// ".concat(d.path, "\n$").concat(d.name, ": ").concat(d.color, ";\n");
+    return ColorArray.push(string);
+  });
+  ColorArray.sort();
+  ColorArray = ColorArray.toString();
+  ColorArray = ColorArray.split(",");
+  colors.forEach(function (d, i) {
+    if (ColorArray[i].includes("// ".concat(d.path))) {
+      return ColorArray[i].replace("// ".concat(d.path), 'lol');
+    }
+  });
+  ColorArray = ColorArray.join("\n");
+  var fileContent = ColorArray;
+  fs.writeFileSync("/Users/marvinbruns/Desktop/fills.scss", fileContent);
+  sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message("generated scss file. Take a look at your desktop.");
+});
+
+function uniq(a) {
+  var seen = {};
+  return a.filter(function (item) {
+    seen.hasOwnProperty(item) ? false : seen[item] = true;
+  });
+}
+
+/***/ }),
+
+/***/ "buffer":
+/*!*************************!*\
+  !*** external "buffer" ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("buffer");
+
+/***/ }),
+
+/***/ "sketch":
+/*!*************************!*\
+  !*** external "sketch" ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("sketch");
+
+/***/ }),
+
+/***/ "sketch/dom":
+/*!*****************************!*\
+  !*** external "sketch/dom" ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("sketch/dom");
+
+/***/ })
+
+/******/ });
+  if (key === 'default' && typeof exports === 'function') {
+    exports(context);
+  } else {
+    exports[key](context);
+  }
+}
+that['onRun'] = __skpm_run.bind(this, 'default')
+
+//# sourceMappingURL=to-scss-var.js.map
